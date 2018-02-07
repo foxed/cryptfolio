cd1804e4c8c2eff9b75431f091ee05b36cfd16bf
foxed <alexdysonfox@gmail.com>
2018-02-07 16:31:22 -0500

    basic form

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import SimplePieChart from './pie';
+import ChartInput from './ChartInput'
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<SimplePieChart />, document.getElementById('root'));
+ReactDOM.render(<ChartInput />, document.getElementById('chartinput'));
 registerServiceWorker();
