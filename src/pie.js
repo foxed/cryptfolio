c3bfefd22459118326d1766e273086cdd82d7849
foxed <alexdysonfox@gmail.com>
2018-02-12 18:40:09 -0500

    minor cleanup

--- src/pie.js
+++ src/pie.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { PieChart, Pie, Tooltip } from 'recharts';
 
 const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
 	                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
 	                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
 
-const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
+const data02 = [{name: 'Group A', value: <input type="text" name="name" />}, {name: 'Group B', value: 4567},
 	                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
 	                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
 
