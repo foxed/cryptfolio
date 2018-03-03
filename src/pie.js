ca71140d643683183d912a357cbad4f3b05f4104
foxed <alexdysonfox@gmail.com>
2018-03-02 20:21:47 -0500

    WIP

--- src/pie.js
+++ src/pie.js
@@ -1,52 +1,68 @@
 import React from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
 import { PieChart, Pie, Tooltip } from 'recharts';
 
 //const data00 = [{name: "one", value: 400}, {name: "two", value: 600}];
+
+class UserInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {sectorAmount: [] };
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleSubmit(event) {
+		this.setState({sectorAmount: [event.target.value]});
+		console.log(this.state.sectorAmount);
+	};
+
+	handleChange(event) {
+		console.log(this.state.sectorAmount);
+	};
+	
+
+	render() {
+		return(
+			   <form onSubmit={this.handleSubmit}>
+						<input type="text"
+							name="sectorAmount"
+							value={this.state.sectorAmount}
+							onChange={this.handleChange} />
+					 <input type="submit" />
+				</form>
+		)};
+}
 
 class TestPie extends React.Component {	
 	constructor(props) {
     super(props);
 		this.state = {
-			sectorAmt: []
+			sectorAmount: []
 		};
-
-	  this.handleChange = this.handleChange.bind(this);
-	  this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	handleChange(event) {
-	  console.log('changed');
-		this.setState({sectorAmt: [event.target.value]});
-	}
-
-	handleSubmit(event) {
-		console.log('submitted');
-    event.preventDefault();
-		console.log(this.state.sectorAmt);
-	}
+handleSectorAmount = (value) => {
+	this.setState({sectorAmount: value});
+	console.log(this.state.sectorAmount);
+};
 
   render() {
 		return (
-			<div>
-			  <div>
-			  <form onSubmit={this.handleSubmit}>
-				  <input type="text"
-						value={this.state.value}
-				    onChange={this.handleChange} /> 
- 			      <input type="submit" value="Add coin" />
-		    </form>
-			  </div>
-			  <div>
-          <PieChart width={800} height={400}>
-			      <Pie dataKey="value" data={this.state.value} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#84ca6f"/>
-				  <Tooltip/>
+        <div>
+				  <UserInput/>
+				  <PieChart width={800} height={400}>
+			      <Pie dataKey="value" 
+								 data={this.state.sectorAmount}
+								 cx={500}
+								 cy={200}
+								 innerRadius={40}
+								 outerRadius={80}
+								 fill="#84ca6f"/>
+				    <Tooltip />
 			    </PieChart>
 		   	</div>
-			</div>
-		);
-	}
+		)};
 }
 
 class Body extends React.Component {
