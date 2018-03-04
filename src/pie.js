ff754c89b8dc9a004c06f6091067fbfeb8c0d28d
Jamon Holmgren <jamon@infinite.red>
2018-03-04 13:19:50 -0800

    Minor formatting tweaks using prettier

--- src/pie.js
+++ src/pie.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { PieChart, Pie, Tooltip } from 'recharts';
 
-//const data00 = [{name: "one", value: 400}, {name: "two", value: 600}];
+// const data00 = [{name: "one", value: 400}, {name: "two", value: 600}];
 
 class UserInput extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {sectorAmount: [] };
+		this.state = { sectorAmount: [] };
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,57 +15,59 @@ class UserInput extends React.Component {
 		event.preventDefault();
 	};
 
-	handleChange(event){
-		this.setState({sectorAmount: event.target.value});
+	handleChange(event) {
+		this.setState({ sectorAmount: event.target.value });
 		console.log(this.state.sectorAmount);
 	}
 
 	render() {
-		return(
+		return (
 			<form onSubmit={this.handleSubmit}>
 				<input type="text"
 					name="sectorAmount"
 					onChange={this.handleChange}
-					value={this.onChange}/>
+					value={this.onChange} />
 				<input type="submit" />
 			</form>
-		)};
+		)
+	};
 }
 
-class TestPie extends React.Component {	
+class TestPie extends React.Component {
 	constructor(props) {
-    super(props);
+		super(props);
 		this.state = {
 			sectorAmount: []
 		};
 		this.handleSectorAmount = this.handleSectorAmount.bind(this);
 	}
 
-handleSectorAmount = (value) => {
-	this.setState({sectorAmount: value});
-	console.log(this.state.sectorAmount);
-};
+	handleSectorAmount = (value) => {
+		this.setState({ sectorAmount: value });
+		console.log(this.state.sectorAmount);
+	};
 
-  render() {
+	render() {
 		return (
 			<div>
-				<UserInput onChange={this.handleSectorAmount}/>
+				<UserInput onChange={this.handleSectorAmount} />
 				<PieChart width={800} height={400}>
-					<Pie dataKey="value" 
-							 data={this.state.sectorAmount}
-							 cx={500}
-							 cy={200}
-							 innerRadius={40}
-							 outerRadius={80}
-							 fill="#84ca6f"/>
+					<Pie dataKey="value"
+						data={this.state.sectorAmount}
+						cx={500}
+						cy={200}
+						innerRadius={40}
+						outerRadius={80}
+						fill="#84ca6f" />
 					<Tooltip />
 				</PieChart>
-		  </div>
-		)};
+			</div>
+		)
+	};
 }
 
 class Body extends React.Component {
-	render () {
+	render() {
 		return (
 			<div>
 				<TestPie />
