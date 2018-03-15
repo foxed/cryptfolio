05165a2f6ff81157a92015ca426b0d7c3ace0e82
foxed <alexdysonfox@gmail.com>
2018-03-15 17:17:09 -0400

    adding basic css grid

--- src/pie.js
+++ src/pie.js
@@ -4,6 +4,34 @@ import { css } from 'glamor';
 import glamorous from 'glamorous';
 import Button from './StyledButton';
 
+const MainGrid = glamorous.div({
+	margin: 'auto',
+	backgroundColor: '#fff',
+	color: '#444',
+	'@supports (display: grid)':{
+		display: 'grid',
+		gridGap: 10,
+		gridTemplateAreas: `
+		"....... header header"
+		"sidebar content content"
+		"footer footer footer"
+		`,
+	},
+});
+
+const Box = glamorous.div({
+	  backgroundColor: '#444',
+	  color: '#fff',
+	  borderRadius: 5,
+	  padding: 10,
+	  fontSize: '150%',
+});
+
+const HeaderFooter = glamorous(Box)({
+	  backgroundColor: '#999',
+});
+
+
 class UserInput extends React.Component {
 	render() {
 		return (
@@ -22,7 +50,7 @@ class UserInput extends React.Component {
 }
 
 class TestPie extends React.Component {
-  constructor(props) {
+	constructor(props) {
 		super(props);
 		// this part creates a data object for this.state.sectors
 		this.state = {
@@ -30,8 +58,8 @@ class TestPie extends React.Component {
 			adding: false,
 			sectors: [
 				{ name: '', value: 0},
-		  ]
-	  };
+			]
+		};
 	}
 
 	handleSectorAmount = (event) => {
@@ -42,7 +70,7 @@ class TestPie extends React.Component {
 		this.setState(state => {
 			// create a new sectors array with this sector's name and value
 			// if it already exists, it'll be updated
-		  const sectors = state.sectors.map(sector => {
+			const sectors = state.sectors.map(sector => {
 				// not current sector, return it unchanged
 				if (sector.name !== sectorName) return sector
 				// current sector, let's return a new one
@@ -57,7 +85,7 @@ class TestPie extends React.Component {
 
 	addSector = () => {
 		this.setState(state => {
-		  return {
+			return {
 				sectors: [
 					...state.sectors,
 					{name: state.newName, value: 0}
@@ -68,39 +96,45 @@ class TestPie extends React.Component {
 	}
 
 	render() {
-	  return (
-		  <div>
-			  {this.state.adding ?
-				  <div>
-					  <input
-					   value={this.state.newName}
-					   placeholder="name"
-					   onChange={ (event) => this.setState({ newName: event.target.value })}/>
-					  <Button
-				     	onClick={this.addSector}>create</Button>
-				  </div>
-					:
+		return (
+			<glamorous.Div maxWidth={600} margin="auto" fontSize={24}>
+		    <MainGrid css={{ marginBottom: 30, marginTop: 20}}>
+				<HeaderFooter css={{ gridArea: 'header' }}>Header</HeaderFooter>
+				<Box css={{ gridArea: 'content'}}>
+
+				{this.state.adding ?
 					<div>
-					  {this.state.sectors.map(sector =>
-					  <UserInput
-						  name={sector.name}
+						<input
+					value={this.state.newName}
+					placeholder="name"
+					onChange={ (event) => this.setState({ newName: event.target.value })}/>
+						<Button
+					onClick={this.addSector}>create</Button>
+						</div>
+						:
+						<div>
+						{this.state.sectors.map(sector =>
+							<UserInput
+							name={sector.name}
 							key={sector.name}
 							value={sector.value}
 							onChange={this.handleSectorAmount} />
-					)}
+						)}
 						<Button type="success" onClick={ () => this.setState({adding: true})}>add</Button>
-				  </div>
+						</div>
 				}
 				<PieChart width={800} height={400}>
 				<Pie dataKey="value"
-			    data={this.state.sectors}
-			    cx={500}
-			    cy={200}
-			    innerRadius={40}
-		      outerRadius={80} />
+			data={this.state.sectors}
+			cx={500}
+			cy={200}
+			innerRadius={40}
+			outerRadius={80} />
 				<Tooltip/>
 				</PieChart>
-			</div>
+				</Box>
+				</MainGrid>
+				</glamorous.Div>
 		)
 	};
 }
