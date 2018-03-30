998aa214e8914177841a3976eca08015421b5d8e
faceyspacey <james@faceyspacey.com>
2018-03-29 21:18:26 -0700

    cleanup to be what I like to see

--- src/pie.js
+++ src/pie.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {PieChart, Pie, Cell, Tooltip} from 'recharts';
-import glamorous from 'glamorous';
-import Button from './StyledButton';
+import React from 'react'
+import {PieChart, Pie, Cell, Tooltip} from 'recharts'
+import glamorous from 'glamorous'
+import Button from './StyledButton'
 
 const MainGrid = glamorous.div({
   margin: 'auto',
@@ -16,7 +16,7 @@ const MainGrid = glamorous.div({
     "footer footer footer"
     `
   }
-});
+})
 
 const Box = glamorous.div({
   backgroundColor: '#d3d3d3',
@@ -25,115 +25,103 @@ const Box = glamorous.div({
   padding: 10,
   fontSize: '150%',
   color: '#45d40C'
-});
+})
 
-const HeaderFooter = glamorous(Box)({textAlign: 'center', fontFamily: 'Courier New', backgroundColor: '#d3d3d3'});
+const HeaderFooter = glamorous(Box)({textAlign: 'center', fontFamily: 'Courier New', backgroundColor: '#d3d3d3'})
 
-const DataDisplay = glamorous.div({fontSize: 24, textAlign: 'left'});
+const DataDisplay = glamorous.div({fontSize: 24, textAlign: 'left'})
 
-const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
 
-class UserInput extends React.Component {
-  render() {
-  return (
-  // this is a "dumb" component
-  // it just shows what we tell it to show, and tells its parent
-  // if it changes through the this.props.onChange function
-   <DataDisplay>
-    <div>
-      <label>{this.props.name}</label>
-    </div>
-    <div>
-      <input type="number"
-        name={this.props.name}
-        value={this.props.value}
-        onChange={this.props.onChange} />
-    </div>
-  </DataDisplay>
-  )
- };
-}
+export default () => <div><TestPie /></div>
+
 
 class TestPie extends React.Component {
   constructor(props) {
-    super(props);
-    // this part creates a data object for this.state.sectors
+    super(props)
+
     this.state = {
-      newName: '',
-      adding: false,
-      sectors: [],
-      pendingValue: null
-    };
+      pendingValue: '',
+      pendingName: '',
+      sectors: [],  // this part creates a data object for this.state.sectors
+      adding: false
+    }
   }
-
-  handleSectorAmount = (event) => {
-    // retrieve the name and value from the input
-    const sectorName = event.target.name;
-    const sectorValue = event.target.value;
-
-    this.setState(state => {
-      // create a new sectors array with this sector's name and value if it already
-      // exists, it'll be updated
-      const sectors = state
-        .sectors
-        .map(sector => {
-          // not current sector, return it unchanged
-          if (sector.name !== sectorName) return sector
-            // current sector, let's return a new one
-          return {
-            name: sectorName,
-            value: parseInt(sectorValue, 10)
-          }
-        })
-
-      return {sectors}
-    })
-  };
 
   // MAIN CONCEPTS:
 
   // 1) STORE VALUE + NAME IN SLOTS
+
   setPendingValue = (event) => {
     const { value } = event.target
-    const pendingValue = parseInt(value, 10)
+    const pendingValue = value ? parseInt(value, 10) : ''
     this.setState({ pendingValue })
   }
 
+  // 2b) go to next page (simple! -- it's basically the same as if both fields were on the same page)
+
+  nextStep = () => {
+    this.setState({ adding: true })
+  }
+
   setPendingName = (event) => {
-    const { value: newName } = event.target
-    this.setState({ newName })
+    const { value: pendingName } = event.target
+    this.setState({ pendingName })
   }
 
   // 2) MERGE EVERYTHING TOGETHER LOGICALLY
-  // -- take the 2 values, make a new array item
-  // -- concat with old eray
-  // -- clear old values so NEW SELECTOR input looks empty
+  // - take the 2 values (name + actual value), and make a new array item out of them
+  // - immutably concat onto old array
+  // - clear old values so NEW SELECTOR input looks empty
   
   addSector = () => {
     this.setState(state => {
       return {
         sectors: [
           ...state.sectors, {
-            name: state.newName,
+            name: state.pendingName,
             value: state.pendingValue
           }
         ],
         adding: false,
-        pendingValue: null,
-        newName: ''
+        pendingValue: '',
+        pendingName: ''
       }
     })
   }
 
-  render() {
-    const { pendingValue: value, sectors } = this.state
+  // 3) ADVANCED (edit existing values -- this is what you originally had, just renamed to be more informative)
 
-    // FOR THE PIE CHARTS, merge the arrays on render every time
-    // at the end of submitting one, there is no value, so u can just show all sectors
-    // but when ur adding one, we basically gotta perform the same merge the finalizes it
-    // but just kinda temporarily
-    const allSectors = value
-      ? sectors.concat({ name: 'NEW VALUE', value })
+  editCurrentValue = (event) => {
+    const { name, value } = event.target
+
+    this.setState(state => {
+      const sectors = state.sectors.map(sector => {
+        if (sector.name !== name) return sector // not current sector, return it unchanged
+        
+        // current sector, let's return a new one
+        return {
+          name,
+          value: value ? parseInt(value, 10) : ''
+        }
+      })
+
+      return { sectors }
+    })
+  }
+
+  render() {
+    const { sectors, pendingValue, pendingName } = this.state
+
+    // FOR THE PIE CHARTS, merge the arrays on render every time.
+    //
+    // This is like what we do in `addSector`, but the difference is the state hasn't changed.
+    // Instead we're temporarily merging existing states to produce a *potential* nextState. That's kinda
+    // the power of this state-driven/declarative stuff. It's highly predictable + controllable 
+    // in the UI where it's possible for far-reaching parts of your app to get tangled together.
+
+    const allSectors = pendingValue
+      ? sectors.concat({ name: 'NEW VALUE', value: pendingValue })
       : sectors
 
     return (
@@ -150,37 +138,39 @@ class TestPie extends React.Component {
           }}>
             {this.state.adding
               ? <div>
-                  <input
+                  <span>STEP 2 - SET NAME</span>
+
+                  <UserInput
                     placeholder="name"
-                    value={this.state.newName}
+                    value={pendingName}
                     onChange={this.setPendingName}
                   />
 
                   <Button onClick={this.addSector}>create</Button>
                 </div>
               : <div>
-                  {this
-                    .state
-                    .sectors
-                    .map(sector => (
-                      <UserInput
-                        name={sector.name}
-                        key={sector.name}
-                        value={sector.value}
-                        onChange={this.handleSectorAmount}
-                      />
-                    ))
-                  }
-
-                  <br /> <br /> <br />
+                  <span>STEP 1 - SET VALUE</span>
 
                   <UserInput
-                    name='NEW SECTOR'
-                    value={value || ''}
+                    type='number'
+                    value={pendingValue}
                     onChange={this.setPendingValue}
                   />
 
-                  <Button type="success" onClick={() => this.setState({adding: true})}>add</Button>
+                  <Button onClick={this.nextStep}>add</Button>
+
+                  <br /> <br />
+                  
+                  {sectors.map(sector => (
+                      <UserInput
+                        type='number'
+                        name={sector.name}
+                        key={sector.name}
+                        value={sector.value}
+                        onChange={this.editCurrentValue}
+                      />
+                    ))
+                  }           
                 </div>
 }
             <PieChart width={800} height={400}>
@@ -190,8 +180,11 @@ class TestPie extends React.Component {
                 cx={500}
                 cy={200}
                 innerRadius={40}
-                outerRadius={80}>
-                {allSectors.map((entry, index) => <Cell key={entry.name} fill={colors[index % colors.length]}/>)}
+                outerRadius={80}
+              >
+                {allSectors.map((entry, index) => (
+                  <Cell key={entry.name} fill={colors[index % colors.length]}/>
+                ))}
               </Pie>
               <Tooltip/>
             </PieChart>
@@ -199,17 +192,19 @@ class TestPie extends React.Component {
         </MainGrid>
       </glamorous.Div>
     )
-  };
-}
-
-class Body extends React.Component {
-  render() {
-    return (
-      <div>
-        <TestPie/>
-      </div>
-    );
   }
 }
 
-export default Body;
+
+class UserInput extends React.Component {
+  render() {
+    return (
+      <DataDisplay>
+        {this.props.name && <label>{this.props.name}</label>}
+        <input {...this.props} type={this.props.type || 'text'} />
+      </DataDisplay>
+    )
+  }
+}
+
+
